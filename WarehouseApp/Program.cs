e2820072e795eb7b9620732d38aa94d095b7b316
Aesir1 <bryan.fatjo@gmail.com>
2022-11-09 16:59:10 +0100

    feat: new project for application logic and abstraction database es working through dependency inversion

--- WarehouseApp/Program.cs
+++ WarehouseApp/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using WarehouseInfrastructure.Contexts;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WarehouseDbContext>(optionsAction =>
+    optionsAction.UseSqlServer("name=ConnectionStrings:DefaultConnection"));
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ WarehouseInfrastructure/Migrations/20221109150143_ContextInjection.cs
@@ -0,0 +1,279 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WarehouseInfrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ContextInjection : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Addresses",
+                columns: table => new
+                {
+                    CodeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTimeUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Addresses", x => x.CodeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTimeUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Guid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dimensions",
+                columns: table => new
+                {
+                    CodeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Length = table.Column<decimal>(type: "decimal(9,4)", precision: 9, scale: 4, nullable: false),
+                    Width = table.Column<decimal>(type: "decimal(9,4)", precision: 9, scale: 4, nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTimeUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dimensions", x => x.CodeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Heavinesses",
+                columns: table => new
+                {
+                    CodeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Weight = table.Column<double>(type: "float", nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTimeUpdated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Heavinesses", x => x.CodeId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Permissions",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserManager = table.Column<bool>(type: "bit", nullable: false),
+                    Delete = table.Column<bool>(type: "bit", nullable: false),
+                    Modified = table.Column<bool>(type: "bit", nullable: false),
+                    Create = table.Column<bool>(type: "bit", nullable: false),
+                    Read = table.Column<bool>(type: "bit", nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTimeUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Permissions", x => x.Guid);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserInfos",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Street = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Nr = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostalCode = table.Column<int>(type: "int", nullable: false),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Country = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserInfos", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DimensionCodeId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    HeavinessCodeId = table.Column<string>(type: "nvarchar(450)", nullable: true),
+                    DateTimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTimeUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Guid);
+                    table.ForeignKey(
+                        name: "FK_Articles_Dimensions_DimensionCodeId",
+                        column: x => x.DimensionCodeId,
+                        principalTable: "Dimensions",
+                        principalColumn: "CodeId");
+                    table.ForeignKey(
+                        name: "FK_Articles_Heavinesses_HeavinessCodeId",
+                        column: x => x.HeavinessCodeId,
+                        principalTable: "Heavinesses",
+                        principalColumn: "CodeId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Guid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PermissionGuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTimeUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Guid);
+                    table.ForeignKey(
+                        name: "FK_Users_Permissions_PermissionGuid",
+                        column: x => x.PermissionGuid,
+                        principalTable: "Permissions",
+                        principalColumn: "Guid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Users_UserInfos_Guid",
+                        column: x => x.Guid,
+                        principalTable: "UserInfos",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Containers",
+                columns: table => new
+                {
+                    ContainerId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Qty = table.Column<int>(type: "int", nullable: false),
+                    AddressCodeId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    ArticleGuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    DateTimeCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DateTimeUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Containers", x => x.ContainerId);
+                    table.ForeignKey(
+                        name: "FK_Containers_Addresses_AddressCodeId",
+                        column: x => x.AddressCodeId,
+                        principalTable: "Addresses",
+                        principalColumn: "CodeId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Containers_Articles_ArticleGuid",
+                        column: x => x.ArticleGuid,
+                        principalTable: "Articles",
+                        principalColumn: "Guid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DepartmentUser",
+                columns: table => new
+                {
+                    DepartmentsGuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UsersGuid = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DepartmentUser", x => new { x.DepartmentsGuid, x.UsersGuid });
+                    table.ForeignKey(
+                        name: "FK_DepartmentUser_Departments_DepartmentsGuid",
+                        column: x => x.DepartmentsGuid,
+                        principalTable: "Departments",
+                        principalColumn: "Guid",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DepartmentUser_Users_UsersGuid",
+                        column: x => x.UsersGuid,
+                        principalTable: "Users",
+                        principalColumn: "Guid",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_DimensionCodeId",
+                table: "Articles",
+                column: "DimensionCodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_HeavinessCodeId",
+                table: "Articles",
+                column: "HeavinessCodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Containers_AddressCodeId",
+                table: "Containers",
+                column: "AddressCodeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Containers_ArticleGuid",
+                table: "Containers",
+                column: "ArticleGuid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DepartmentUser_UsersGuid",
+                table: "DepartmentUser",
+                column: "UsersGuid");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_PermissionGuid",
+                table: "Users",
+                column: "PermissionGuid");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Containers");
+
+            migrationBuilder.DropTable(
+                name: "DepartmentUser");
+
+            migrationBuilder.DropTable(
+                name: "Addresses");
+
+            migrationBuilder.DropTable(
+                name: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Dimensions");
+
+            migrationBuilder.DropTable(
+                name: "Heavinesses");
+
+            migrationBuilder.DropTable(
+                name: "Permissions");
+
+            migrationBuilder.DropTable(
+                name: "UserInfos");
+        }
+    }
+}
