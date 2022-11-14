71bb403a384fe21264368e0bee8fac342c76346a
Aesir1 <bryan.fatjo@gmail.com>
2022-11-14 15:07:21 +0100

    fix: migration adjust

--- WarehouseInfrastructure/Migrations/20221114134537_ContextChange.Designer.cs
+++ WarehouseInfrastructure/Migrations/20221114134537_ContextChange.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WarehouseInfrastructure.Contexts;
 
@@ -11,9 +12,11 @@ using WarehouseInfrastructure.Contexts;
 namespace WarehouseInfrastructure.Migrations
 {
     [DbContext(typeof(WarehouseDbContext))]
-    partial class WarehouseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221114134537_ContextChange")]
+    partial class ContextChange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
